ae2daed2801699795adc4a6f9c17050314133748
Shai Rosenfeld <shaiguitar@gmail.com>
2015-03-09 21:24:26 -0700

    update to 4.3.24 p98716

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,8 +4,8 @@
 #
 #   include virtualbox
 class virtualbox (
-  $version = '4.3.14',
-  $patch_level = '95030'
+  $version = '4.3.24',
+  $patch_level = '98716'
 ) {
 
   exec { 'Kill Virtual Box Processes':
